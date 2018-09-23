6fc3b92c444eac24cc26b6cf7af7052ac4d64eb5
Jucinara Dantas <30300296+jucinaradantas@users.noreply.github.com>
2018-09-22 21:49:41 -0300

    Tela Principal. Todas as telas, ainda em branco

--- app/src/main/java/br/edu/ifsp/bdhouse/BDHActivity.java
+++ app/src/main/java/br/edu/ifsp/bdhouse/BDHActivity.java
@@ -17,9 +17,49 @@ public class BDHActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bdh);
     }
 
+    public void lauchUsuario(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, UsuarioActivity.class);
+        startActivity(intent);
+    }
     public void lauchVisitas(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, VisitaActivity.class);
+        startActivity(intent);
+    }
+    public void lauchAlarme(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, AlarmeActivity.class);
+        startActivity(intent);
+    }
+    public void lauchLuzes(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, LuzesActivity.class);
+        startActivity(intent);
+    }
+    public void lauchEletrodomestico(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, EletrodomesticoActivity.class);
+        startActivity(intent);
+    }
+    public void lauchPortao(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this,PortaoActivity.class);
+        startActivity(intent);
+    }
+    public void lauchTeto(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this,TetoActivity.class);
+        startActivity(intent);
+    }
+    public void lauchSolo(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this,SoloActivity.class);
+        startActivity(intent);
+    }
+    public void lauchAr(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this,ArActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/br/edu/ifsp/bdhouse/SoloActivity.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bdhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solo);
+    }
+}
